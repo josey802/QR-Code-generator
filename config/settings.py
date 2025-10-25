a1211e6d3f8d8ec9e6c5a62e93b02038a09d0ea0
Joseph Ayoola <theayoolajoseph@gmail.com>
2025-10-25 14:45:18 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -123,4 +123,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
